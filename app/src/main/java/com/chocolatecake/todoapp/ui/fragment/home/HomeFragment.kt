bb46c48a79eaba6432ddc668c6646ad6b9a1fe2a
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-04-10 23:16:26 +0200

    remove home fragment

--- app/src/main/java/com/chocolatecake/todoapp/ui/fragment/home/HomeFragment.kt
+++
@@ -1,7 +0,0 @@
-package com.chocolatecake.todoapp.ui.fragment.home
-
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import com.chocolatecake.todoapp.databinding.FragmentHomeBinding
-import com.chocolatecake.todoapp.ui.fragment.base.BaseFragment
-
