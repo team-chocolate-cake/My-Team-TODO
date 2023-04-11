c94516cd2d58b91a59fce5fd5776529aaa3e3398
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-04-11 20:12:11 +0200

    add default value

--- app/src/main/java/com/chocolatecake/todoapp/data/network/services/utils/Utils.kt
+++ app/src/main/java/com/chocolatecake/todoapp/data/network/services/utils/Utils.kt
@@ -3,18 +3,19 @@ package com.chocolatecake.todoapp.data.network.services.utils
 import okhttp3.HttpUrl
 
 object Utils {
-    fun getUrl(path: String, subPath: String): HttpUrl {
+    fun getUrl(path: String, subPath: String = ""): HttpUrl {
         return HttpUrl.Builder()
             .scheme(SCHEME)
             .host(HOST)
             .addPathSegment(path)
             .apply {
-                if (subPath.isNotEmpty()) { // Check if subPath is not empty or null
+                if (subPath.isNotEmpty()) {
                     addPathSegment(subPath)
                 }
             }
             .build()
     }
-    const val SCHEME ="https"
-    const val HOST ="team-todo-62dmq.ondigitalocean.app"
+
+    private const val SCHEME = "https"
+    private const val HOST = "team-todo-62dmq.ondigitalocean.app"
 }
