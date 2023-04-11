6ed1741c34029a31291b55462fe856f3d752fd7e
Hasan ALi <hasan.20cs75@student.uomosul.edu.iq>
2023-04-11 13:58:15 +0300

    make functionality of create account

--- app/src/main/java/com/chocolatecake/todoapp/ui/fragment/base/RegistrationFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/fragment/base/RegistrationFragment.kt
@@ -1,18 +1,22 @@
 package com.chocolatecake.todoapp.ui.fragment.base
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import com.chocolatecake.todoapp.data.model.request.UserRequest
+import com.chocolatecake.todoapp.data.network.services.identity.AuthService
 import com.chocolatecake.todoapp.databinding.FragmentRegisterBinding
 import com.chocolatecake.todoapp.util.Constant
 import com.chocolatecake.todoapp.util.isUsernameValid
 
 class RegistrationFragment : Fragment() {
     private lateinit var binding: FragmentRegisterBinding
+    private var validationUserName : Boolean = false
+    private var validationPassword : Boolean = false
+    private var validationConfirm : Boolean = false
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,7 +26,41 @@ class RegistrationFragment : Fragment() {
         validationOfUsername()
         validationOfPassword()
         matchPassword()
+        checkValidationRegister()
         return binding.root
+    }
+    private fun validationOfUsername() {
+        binding.apply {
+            editTextUsername.addTextChangedListener {
+                when{
+                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_SPECIAL-> {
+                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_SPECIAL
+                        textViewValidateUserName.visibility = View.VISIBLE
+                        validationUserName = false
+                    }
+                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_SPACE-> {
+                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_SPACE
+                        textViewValidateUserName.visibility = View.VISIBLE
+                        validationUserName = false
+                    }
+                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_SHOULD_GRATER_THE_LIMIT-> {
+                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_SHOULD_GRATER_THE_LIMIT
+                        textViewValidateUserName.visibility = View.VISIBLE
+                        validationUserName = false
+                    }
+                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_START_WITH_DIGIT -> {
+                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_START_WITH_DIGIT
+                        textViewValidateUserName.visibility = View.VISIBLE
+                        validationUserName = false
+                    }
+
+                    else -> {
+                        textViewValidateUserName.visibility = View.GONE
+                        validationUserName = true
+                    }
+                }
+            }
+        }
     }
     private fun validationOfPassword() {
         binding.apply {
@@ -32,52 +70,52 @@ class RegistrationFragment : Fragment() {
                     passwordLength < Constant.VALIDATION_PASSWORD_LENGTH -> {
                         textViewValidatePassword.text = Constant.ERROR_VALIDATION_PASSWORD_TEXT_LENGTH
                         textViewValidatePassword.visibility = View.VISIBLE
+                        validationPassword =  false
                     }
-                    else -> textViewValidatePassword.visibility = View.GONE
+                    else -> {
+                        textViewValidatePassword.visibility = View.GONE
+                        validationPassword =  true
+                    }
                 }
             }
         }
     }
-    private fun validationOfUsername(){
-        binding.apply {
-            editTextUsername.addTextChangedListener { editable->
-                when{
-                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_SPECIAL-> {
-                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_SPECIAL
-                        textViewValidateUserName.visibility = View.VISIBLE
-                    }
-                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_SPACE-> {
-                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_SPACE
-                        textViewValidateUserName.visibility = View.VISIBLE
-                    }
-                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_SHOULD_GRATER_THE_LIMIT-> {
-                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_SHOULD_GRATER_THE_LIMIT
-                        textViewValidateUserName.visibility = View.VISIBLE
-                    }
-                    isUsernameValid(editTextUsername.text.toString()) == Constant.ERROR_VALIDATION_USER_NAME_START_WITH_DIGIT -> {
-                        textViewValidateUserName.text = Constant.ERROR_VALIDATION_USER_NAME_START_WITH_DIGIT
-                        textViewValidateUserName.visibility = View.VISIBLE
-                    }
-
-                    else -> textViewValidateUserName.visibility = View.GONE
-                }
-            }
-        }
-    }
-
     private fun matchPassword(){
         binding.apply {
             editTextConfirmPassword.addTextChangedListener {confirmPassword ->
-                if(editTextPassword.text.toString() == editTextConfirmPassword.text.toString())
+                if(editTextPassword.text.toString() == editTextConfirmPassword.text.toString()){
                     textViewValidateConfirm.visibility = View.GONE
+                    validationConfirm = true
+                }
                 else{
                     textViewValidateConfirm.text = Constant.ERROR_VALIDATION_CONFIRM_PASSWORD_MISMATCH
                     textViewValidateConfirm.visibility = View.VISIBLE
+                    validationConfirm = false
                 }
-
             }
         }
     }
+    private fun checkValidationRegister(){
 
+        binding.buttonRegister.setOnClickListener {
+            if(validationUserName && validationPassword && validationConfirm){
+                val auth = AuthService({
+                    TODO("ايرور بطعم الجوكليت كيك")
+                }, {
+                  ""
+                })
+                val username = binding.editTextUsername.text.toString()
+                val password = binding.editTextPassword.text.toString()
+                val newUser = UserRequest(username, password)
+                auth.register(newUser)
+
+            }
+
+
+        }
+
+    }
 
 }
+
+
