16088b308c92ec573f3d97b2b688652339228836
Hasan ALi <hasan.20cs75@student.uomosul.edu.iq>
2023-04-11 06:49:56 +0300

    make functionality of validate password

--- app/src/main/java/com/chocolatecake/todoapp/ui/fragment/base/RegistrationFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/fragment/base/RegistrationFragment.kt
@@ -1,11 +1,15 @@
 package com.chocolatecake.todoapp.ui.fragment.base
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import com.chocolatecake.todoapp.databinding.FragmentRegisterBinding
+import com.chocolatecake.todoapp.util.Constant
 
 
 class RegistrationFragment : Fragment(){
@@ -16,6 +20,25 @@ class RegistrationFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        validationOfPassword()
         return binding.root
+    }
+
+    private fun validationOfPassword() {
+        binding.apply {
+            editTextPassword.addTextChangedListener { passwordText ->
+                val passwordLength = passwordText!!.length
+                when {
+                    passwordLength < Constant.VALIDATION_PASSWORD_LENGTH -> {
+                        textViewValidate.text = Constant.ERROR_VALIDATION_PASSWORD_TEXT
+                        textViewValidate.visibility = View.VISIBLE
+                    }
+                    else -> textViewValidate.visibility = View.GONE
+                }
+            }
+        }
+    }
+    private fun checkTheRegisterValidation(){
+
     }
 }
--- app/src/main/java/com/chocolatecake/todoapp/util/Constant.kt
+++ app/src/main/java/com/chocolatecake/todoapp/util/Constant.kt
@@ -1,4 +1,7 @@
 package com.chocolatecake.todoapp.util
 
 object Constant {
+
+    const val VALIDATION_PASSWORD_LENGTH = 8
+    const val ERROR_VALIDATION_PASSWORD_TEXT = "Please should be greater than 8 character"
 }
