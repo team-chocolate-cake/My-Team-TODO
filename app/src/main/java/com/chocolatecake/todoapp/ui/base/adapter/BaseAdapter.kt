6a658ca4c932f379fbdac661d5f737f373df9f8f
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-04-11 22:00:17 +0200

    change packages

--- app/src/main/java/com/chocolatecake/todoapp/ui/base/adapter/BaseAdapter.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/base/adapter/BaseAdapter.kt
@@ -1,4 +1,4 @@
-package com.chocolatecake.todoapp.ui.adapter
+package com.chocolatecake.todoapp.ui.base.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
--- app/src/main/java/com/chocolatecake/todoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/home/HomeFragment.kt
@@ -1,9 +1,9 @@
-package com.chocolatecake.todoapp.ui.fragment.home
+package com.chocolatecake.todoapp.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.chocolatecake.todoapp.databinding.FragmentHomeBinding
-import com.chocolatecake.todoapp.ui.fragment.base.BaseFragment
+import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding =
