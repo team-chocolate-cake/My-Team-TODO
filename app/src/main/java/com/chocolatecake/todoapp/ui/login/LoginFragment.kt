cb92c5258611a8d2e20aa2a75274093cf92fd22b
jinan <jinanaqb@gmail.com>
2023-04-13 02:30:15 +0300

    add mvp for login

--- app/src/main/java/com/chocolatecake/todoapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/login/LoginFragment.kt
@@ -1,33 +1,50 @@
 package com.chocolatecake.todoapp.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.chocolatecake.todoapp.R
+import com.chocolatecake.todoapp.data.model.request.UserRequest
 import com.chocolatecake.todoapp.databinding.FragmentLoginBinding
 import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
+import com.chocolatecake.todoapp.ui.login.presenter.LoginPresenter
 
 
-class LoginFragment : BaseFragment<FragmentLoginBinding>() {
+class LoginFragment : BaseFragment<FragmentLoginBinding>() , LoginView{
+
+    private var presenter: LoginPresenter = LoginPresenter(this)
+    val userRequest : UserRequest = UserRequest(username = "Jinan" , password = "123457777")
 
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentLoginBinding
         get() = FragmentLoginBinding::inflate
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
+        setUp()
+    }
 
+    private fun setUp() {
+        binding.buttonLogin.setOnClickListener {
+            presenter.clickableLoginButton(userRequest)
         }
     }
 
-    companion object {
-        fun newInstance() =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                }
-            }
+    override fun onFailureLogin(message: String?) {
+        Log.i("login" ,"message onFailureLogin" + message.toString())
     }
+
+
+    override fun onSuccessLogin() {
+
+    }
+
+    override fun onSuccessResponse(message: String?) {
+        Log.i("login" ,"message onSuccessResponse" + message.toString())
+    }
+
 
 }
