f995bd0ae355a8773fd7863ecf5194d3b24c9369
Amna <amna.alsikafi@gmail.com>
2023-04-12 05:23:03 -0700

    implement home screen

--- app/src/main/java/com/chocolatecake/todoapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/home/HomeFragment.kt
@@ -2,10 +2,38 @@ package com.chocolatecake.todoapp.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.chocolatecake.todoapp.R
 import com.chocolatecake.todoapp.databinding.FragmentHomeBinding
 import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding =
         FragmentHomeBinding::inflate
+
+    private fun addCallBacks() {
+        binding.floatingActionButton.setOnClickListener {
+//            val addNewTaskFragment = AddNewTaskFragment.newInstance(true)
+//            replaceFragment(addNewTaskFragment)
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = requireActivity().supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container_view, fragment).addToBackStack(null)
+        fragmentTransaction.commit()
+    }
+
+    private fun checkUserSection(section:Int):Boolean{
+    return if(section==IS_PERSONAL){
+       true
+    }  else if (section==IS_TEAM){
+        false
+    } else false
+    }
+   companion object{
+       val IS_TEAM = 1
+       val IS_PERSONAL = 2
+   }
 }
