fd728a06226cef8eefaa9e23b2e8cd7839c41bc0
nahed <nahedshehata>
2023-04-12 19:51:09 +0200

    add lagic task detials fragment

---
+++ app/src/main/java/com/chocolatecake/todoapp/ui/fragment/base/TaskDetialsFragment.kt
@@ -0,0 +1,10 @@
+package com.chocolatecake.todoapp.ui.fragment.base
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.chocolatecake.todoapp.databinding.FragmentTaskDetailsBinding
+
+class TaskDetialsFragment:BaseFragment<FragmentTaskDetailsBinding>() {
+    override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentTaskDetailsBinding
+        get() = TODO("Not yet implemented")
+}
