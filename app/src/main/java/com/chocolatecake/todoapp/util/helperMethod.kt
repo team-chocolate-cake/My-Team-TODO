c4a8f1bc328df5ff64eaa19491a827980814e976
azmoh09 <momoo362514@gmail.com>
2023-04-13 00:57:24 +0200

    edit: strings

--- app/src/main/java/com/chocolatecake/todoapp/util/helperMethod.kt
+++ app/src/main/java/com/chocolatecake/todoapp/util/helperMethod.kt
@@ -1,15 +1,21 @@
 package com.chocolatecake.todoapp.util
 
-import com.chocolatecake.todoapp.ui.fragment.base.RegistrationFragment
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import com.chocolatecake.todoapp.R
+import com.chocolatecake.todoapp.ui.register.RegistrationFragment
 import java.lang.Character.isDigit
 
-fun getUsernameStatus(username : String): String {
+fun getUsernameStatus(username : String,context : Context): String {
     for(i in username.trim().indices){
         when {
-            isSpecial(username[i]) -> return RegistrationFragment.ERROR_VALIDATION_USER_NAME_SPECIAL
-            isSpace(username[i]) -> return RegistrationFragment.ERROR_VALIDATION_USER_NAME_SPACE
-            isDigit(username[0]) -> return RegistrationFragment.ERROR_VALIDATION_USER_NAME_START_WITH_DIGIT
-            username.length < RegistrationFragment.VALIDATION_USERNAME_LENGTH -> return RegistrationFragment.ERROR_VALIDATION_USER_NAME_SHOULD_GRATER_THE_LIMIT
+
+            isSpecial(username[i]) -> return context.getString(R.string.error_validation_user_name_special)
+            isSpace(username[i]) -> return  context.getString(R.string.error_validation_user_name_space)
+            isDigit(username[0]) -> return context.getString(R.string.error_validation_user_name_should_grater_the_limit)
+            username.length < RegistrationFragment.VALIDATION_USERNAME_LENGTH -> return context.getString(R.string.error_validation_user_name_start_with_digit)
+
         }
     }
     return ""
