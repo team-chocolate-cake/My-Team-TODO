db69c831a365e9ea9960b67a369ad0f38cf0fc5c
Ahmed Ghannam <iia7mt@gmail.com>
2023-04-13 15:19:32 +0300

    reformat the code

--- app/src/main/java/com/chocolatecake/todoapp/ui/fragment/add_new_task/AddNewTaskFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/fragment/add_new_task/AddNewTaskFragment.kt
@@ -7,94 +7,148 @@ import android.view.ViewGroup
 import android.widget.Toast
 import com.chocolatecake.todoapp.R
 import com.chocolatecake.todoapp.databinding.FragmentAddNewTaskBinding
+
 import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
 
 class AddNewTaskFragment : BaseFragment<FragmentAddNewTaskBinding>(), AddNewTaskView {
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentAddNewTaskBinding
         get() = FragmentAddNewTaskBinding::inflate
 
-    private lateinit var presenter: AddNewTaskPresenter
+    private val presenter: AddNewTaskPresenter by lazy {
+        AddNewTaskPresenter(requireContext(), this)
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        customizeLayout(retrieveTypeFromArguments())
-        presenter = AddNewTaskPresenter(requireContext(), this)
-        setUp()
+        val isPersonal = retrieveTypeFromArguments()
+        setPersonalOrTeamLayout(isPersonal)
+        addCallBacks()
     }
-    private fun setUp() {
+
+    private fun addCallBacks() {
         binding.buttonAdd.setOnClickListener {
-            val isPersonal = retrieveTypeFromArguments()
-            checkInputField(isPersonal)
-            pushTaskToApi()
-            activity?.supportFragmentManager?.popBackStack()
+            if (isInputValid()) {
+                showBottomSheetDialog()
+            } else {
+                createToast(getString(R.string.please_fill_fields))
+            }
         }
         binding.appbar.setNavigationOnClickListener {
-            parentFragmentManager.popBackStack()
+            returnToHomeFragment()
         }
     }
-    private fun customizeLayout(isPersonal: Boolean) {
+
+    private fun showBottomSheetDialog() {
+        CreateTaskConfirmDialog(::createTask)
+            .show(childFragmentManager, BOTTOM_SHEET_DIALOG)
+    }
+
+    private fun createTask() {
+        createAndPushTaskToApi()
+        returnToHomeFragment()
+    }
+
+    private fun isInputValid(): Boolean {
+        val isPersonal = retrieveTypeFromArguments()
+        return if (isPersonal) {
+            isPersonalInputValid()
+        } else {
+            isTeamInputValid()
+        }
+    }
+
+    private fun isPersonalInputValid(): Boolean {
+        binding.apply {
+            val title = editTextTitle.text.toString().trim()
+            val description = editTextDescription.text.toString().trim()
+            return noStringIsEmpty(title, description)
+        }
+    }
+
+    private fun isTeamInputValid(): Boolean {
+        binding.apply {
+            val title = editTextTitle.text.toString().trim()
+            val description = editTextDescription.text.toString().trim()
+            val assignee = editTextAssignee.text.toString().trim()
+            return noStringIsEmpty(title, description, assignee)
+        }
+    }
+
+    private fun noStringIsEmpty(vararg s: String): Boolean {
+        return s.all {
+            it.isNotEmpty()
+        }
+    }
+
+    private fun returnToHomeFragment() {
+        parentFragmentManager.popBackStack()
+    }
+
+    private fun setPersonalOrTeamLayout(isPersonal: Boolean) {
         binding.apply {
             if (isPersonal) {
-                editTextAssignee.visibility = View.GONE
-                textViewAssignee.visibility = View.GONE
+                removeAssigneeInputField()
             } else {
                 appbar.title = resources.getString(R.string.title_add_new_team_task)
-                editTextAssignee.visibility = View.VISIBLE
-                textViewAssignee.visibility = View.VISIBLE
+                showAssigneeInputField()
             }
         }
     }
-    private fun checkInputField(isPersonal: Boolean) {
-        if (!isPersonal) {
-            binding.apply {
-                val title = editTextTitle.text.toString().trim()
-                val description = editTextDescription.text.toString().trim()
-                val assignee = editTextAssignee.text.toString().trim()
-                if (title.isEmpty() || description.isEmpty() && assignee.isEmpty()) {
-                    createToast("please fill all fields")
-                }
-            }
-        } else {
-            binding.apply {
-                val title = editTextTitle.text.toString().trim()
-                val description = editTextDescription.text.toString().trim()
-                if (title.isEmpty() || description.isEmpty()) {
-                    createToast("please fill all fields")
-                }
-            }
+
+    private fun showAssigneeInputField() {
+        binding.apply {
+            editTextAssignee.visibility = View.VISIBLE
+            textViewAssignee.visibility = View.VISIBLE
         }
     }
-    private fun pushTaskToApi() {
+
+    private fun removeAssigneeInputField() {
+        binding.apply {
+            editTextAssignee.visibility = View.GONE
+            textViewAssignee.visibility = View.GONE
+        }
+    }
+
+    private fun createAndPushTaskToApi() {
         val title = binding.editTextTitle.text.toString().trim()
         val description = binding.editTextDescription.text.toString().trim()
         val assignee = binding.editTextAssignee.text.toString().trim()
+
         if (retrieveTypeFromArguments()) {
             presenter.createPersonalTask(title, description)
         } else {
             presenter.createTeamTask(title, description, assignee)
         }
     }
+
+
+    private fun retrieveTypeFromArguments(): Boolean {
+        return arguments?.getBoolean(IS_PERSONAL.toString(), true)!!
+    }
+
+    private fun createToast(message: String?) {
+        context?.let {
+            Toast.makeText(it, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateTaskFailure() {
+        createToast("Failure")
+    }
+
+    override fun onCreateTaskSuccess() {
+        createToast("Success")
+    }
+
     companion object {
-        const val IS_PERSONAL = true
+        private const val IS_PERSONAL = true
+        private const val BOTTOM_SHEET_DIALOG = "newTaskTag"
         fun newInstance(isPersonal: Boolean) =
             AddNewTaskFragment().apply {
                 arguments = Bundle().apply {
                     putBoolean(IS_PERSONAL.toString(), isPersonal)
                 }
             }
-    }
-    private fun retrieveTypeFromArguments(): Boolean {
-        return arguments?.getBoolean(IS_PERSONAL.toString(), true)!!
-    }
-    private fun createToast(message: String?) {
-        context?.let {
-            Toast.makeText(it, message, Toast.LENGTH_SHORT).show()
-        }
-    }
-    override fun onCreateTaskFailure() {
-        createToast("Failure")
-    }
-    override fun onCreateTaskSuccess() {
-        createToast("Success")
     }
 }
