e0ead5f22faf5dfeda7f9a8701bee139c0b90501
Ahmed Ghannam <iia7mt@gmail.com>
2023-04-13 12:42:50 +0300

    refactor the code

--- app/src/main/java/com/chocolatecake/todoapp/ui/fragment/add_new_task/AddNewTaskFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/fragment/add_new_task/AddNewTaskFragment.kt
@@ -18,116 +18,172 @@ class AddNewTaskFragment : BaseFragment<FragmentAddNewTaskBinding>() {
 
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentAddNewTaskBinding
         get() = FragmentAddNewTaskBinding::inflate
-    private lateinit var sharedPreferences: TaskSharedPreferences
+
+    private val sharedPreferences by lazy {
+        TaskSharedPreferences().also {
+            it.initPreferences(requireContext())
+            it.token = ""
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        customizeLayout(retrieveTypeFromArguments())
-        sharedPreferences = TaskSharedPreferences()
-        sharedPreferences.initPreferences(requireContext())
-        sharedPreferences.token = ""
+        val isPersonal = retrieveTypeFromArguments()
+        setPersonalOrTeamLayout(isPersonal)
         addCallBacks()
     }
+
+    private fun isUserConfirmedTaskCreation(): Boolean {
+        return true
+    }
+
     private fun addCallBacks() {
         binding.buttonAdd.setOnClickListener {
-            val isPersonal = retrieveTypeFromArguments()
-            checkInputField(isPersonal)
-            pushTaskToApi()
-            activity?.supportFragmentManager?.popBackStack()
+            if (isUserConfirmedTaskCreation()) {
+                createTask()
+            }
         }
         binding.appbar.setNavigationOnClickListener {
-            parentFragmentManager.popBackStack()
+            returnToHomeFragment()
         }
     }
-    private fun customizeLayout(isPersonal: Boolean) {
+
+    private fun createTask() {
+        if (isInputValid()) {
+            pushTaskToApi()
+            returnToHomeFragment()
+        }
+    }
+
+    private fun returnToHomeFragment() {
+        parentFragmentManager.popBackStack()
+    }
+
+    private fun setPersonalOrTeamLayout(isPersonal: Boolean) {
         binding.apply {
             if (isPersonal) {
-                editTextAssignee.visibility = View.GONE
-                textViewAssignee.visibility = View.GONE
+                removeAssigneeInputField()
             } else {
                 appbar.title = resources.getString(R.string.title_add_new_team_task)
-                editTextAssignee.visibility = View.VISIBLE
-                textViewAssignee.visibility = View.VISIBLE
+                showAssigneeInputField()
             }
         }
     }
-    private fun checkInputField(isPersonal: Boolean) {
-        if (!isPersonal) {
-            binding.apply {
-                val title = editTextTitle.text.toString().trim()
-                val description = editTextDescription.text.toString().trim()
-                val assignee = editTextAssignee.text.toString().trim()
-                if (title.isEmpty() || description.isEmpty() && assignee.isEmpty()) {
-                    createToast("please fill all fields")
-                }
-            }
+
+    private fun showAssigneeInputField() {
+        binding.apply {
+            editTextAssignee.visibility = View.VISIBLE
+            textViewAssignee.visibility = View.VISIBLE
+        }
+    }
+
+    private fun removeAssigneeInputField() {
+        binding.apply {
+            editTextAssignee.visibility = View.GONE
+            textViewAssignee.visibility = View.GONE
+        }
+    }
+
+    private fun isInputValid(): Boolean {
+        val isPersonal = retrieveTypeFromArguments()
+        return if (isPersonal) {
+            isPersonalInputValid()
         } else {
-            binding.apply {
-                val title = editTextTitle.text.toString().trim()
-                val description = editTextDescription.text.toString().trim()
-                if (title.isEmpty() || description.isEmpty()) {
-                    createToast("please fill all fields")
-                }
+            isTeamInputValid()
+        }
+    }
+
+    private fun isPersonalInputValid(): Boolean {
+        binding.apply {
+            val title = editTextTitle.text.toString().trim()
+            val description = editTextDescription.text.toString().trim()
+            if (title.isEmpty() || description.isEmpty()) {
+                createToast(getString(R.string.please_fill_fields))
+                return false
             }
         }
+
+        return true
+    }
+
+    private fun isTeamInputValid(): Boolean {
+        binding.apply {
+            val title = editTextTitle.text.toString().trim()
+            val description = editTextDescription.text.toString().trim()
+            val assignee = editTextAssignee.text.toString().trim()
+            if (title.isEmpty() || description.isEmpty() || assignee.isEmpty()) {
+                createToast(getString(R.string.please_fill_fields))
+                return false
+            }
+        }
+        return true
     }
 
     private fun pushTaskToApi() {
         if (retrieveTypeFromArguments()) {
-            createToast(retrieveTypeFromArguments().toString())
             createPersonalTask()
         } else {
-            createToast(retrieveTypeFromArguments().toString())
             createTeamTask()
         }
     }
 
     private fun createPersonalTask() {
-        val personalTaskService = PersonalTaskService(sharedPreferences)
         val personalTaskRequest = createPersonalTaskRequestObject()
+        val personalTaskService = PersonalTaskService(sharedPreferences)
         personalTaskService.createTask(
-            personalTaskRequest, onFailure = ::onCreateTaskFailure,
+            personalTaskRequest,
+            onFailure = ::onCreateTaskFailure,
             onSuccess = ::onCreateTaskSuccess
         )
     }
+
     private fun createTeamTask() {
         val teamTaskService = TeamTaskService(sharedPreferences)
         val teamTaskRequest = createTeamTaskRequestObject()
-        teamTaskService.createTask(teamTaskRequest, onFailure = ::onCreateTaskFailure,
-            onSuccess = ::onCreateTaskSuccess)
+        teamTaskService.createTask(
+            teamTaskRequest, onFailure = ::onCreateTaskFailure,
+            onSuccess = ::onCreateTaskSuccess
+        )
     }
+
     private fun createPersonalTaskRequestObject(): PersonalTaskRequest {
         val title = binding.editTextTitle.text.toString().trim()
         val description = binding.editTextDescription.text.toString().trim()
         return PersonalTaskRequest(title, description)
     }
+
     private fun createTeamTaskRequestObject(): TeamTaskRequest {
         val title = binding.editTextTitle.text.toString().trim()
         val assignee = binding.editTextAssignee.text.toString().trim()
         val description = binding.editTextDescription.text.toString().trim()
         return TeamTaskRequest(title, description, assignee)
     }
+
     private fun onCreateTaskSuccess(body: String?) {
         createToast(body)
     }
+
     private fun onCreateTaskFailure(message: String?) {
         createToast(message)
     }
+
+    private fun retrieveTypeFromArguments(): Boolean {
+        return arguments?.getBoolean(IS_PERSONAL, true)!!
+    }
+
+    private fun createToast(message: String?) {
+        activity?.runOnUiThread {
+            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+        }
+    }
+
     companion object {
-        const val IS_PERSONAL = true
+        const val IS_PERSONAL = "ARE_WE_ADDING_TO_PERSONAL_TASKS"
         fun newInstance(isPersonal: Boolean) =
             AddNewTaskFragment().apply {
                 arguments = Bundle().apply {
-                    putBoolean(IS_PERSONAL.toString(), isPersonal)
+                    putBoolean(IS_PERSONAL, isPersonal)
                 }
             }
-    }
-    private fun retrieveTypeFromArguments(): Boolean {
-        return arguments?.getBoolean(IS_PERSONAL.toString(), true)!!
-    }
-    private fun createToast(message: String?) {
-       activity?.runOnUiThread {
-           Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-       }
     }
 }
