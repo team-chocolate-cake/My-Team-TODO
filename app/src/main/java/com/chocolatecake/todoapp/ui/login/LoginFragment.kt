a42ffe4560d0f72fc9c21ef5d23abcdf6146371e
jinan <jinanaqb@gmail.com>
2023-04-13 18:25:12 +0300

    add show toast

--- app/src/main/java/com/chocolatecake/todoapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/login/LoginFragment.kt
@@ -1,20 +1,21 @@
 package com.chocolatecake.todoapp.ui.login
 
+import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import com.chocolatecake.todoapp.R
 import com.chocolatecake.todoapp.data.model.request.UserRequest
+import com.chocolatecake.todoapp.databinding.ActivityHomeBinding
 import com.chocolatecake.todoapp.databinding.FragmentLoginBinding
+import com.chocolatecake.todoapp.ui.activity.HomeActivity
 import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
 import com.chocolatecake.todoapp.ui.login.presenter.LoginPresenter
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
-    private var presenter: LoginPresenter = LoginPresenter(this)
+    private val presenter by lazy { LoginPresenter(view= this, context= HomeActivity()) }
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentLoginBinding
         get() = FragmentLoginBinding::inflate
 
@@ -36,6 +37,11 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
     override fun onFailureLogin(message: String?) {
         println("hghhgghghghhghghghghghghg"+ message)
         Log.i("login", "message onFailureLogin $message")
+
+        requireActivity().runOnUiThread {
+            showToast(message= message)
+        }
+
     }
 
     override fun onSuccessLogin() {
@@ -43,6 +49,17 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
     }
 
     override fun onFailureResponse(message: String?) {
+        requireActivity().runOnUiThread {
+            showToast(message= message)
+        }
         Log.i("login", "message onFailureResponse $message")
+    }
+
+    private fun showToast(message:String?){
+        Toast.makeText(
+            requireContext(),
+            message,
+            Toast.LENGTH_SHORT
+        ).show()
     }
 }
