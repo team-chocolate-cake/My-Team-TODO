ee1fe7f514d847ce6048217c924d53a045305dcf
azmoh09 <momoo362514@gmail.com>
2023-04-13 13:00:52 +0200

    add: register presenter

--- app/src/main/java/com/chocolatecake/todoapp/presenter/RegistarationPresenter.kt
+++ app/src/main/java/com/chocolatecake/todoapp/presenter/RegistarationPresenter.kt
@@ -1,5 +1,21 @@
 package com.chocolatecake.todoapp.presenter
 
-class RegistrationPresenter{
+import com.chocolatecake.todoapp.data.model.request.UserRequest
+import com.chocolatecake.todoapp.data.model.response.RegisterResponse
+import com.chocolatecake.todoapp.data.network.services.identity.AuthService
+import com.chocolatecake.todoapp.ui.register.IRegisterView
+import com.google.gson.Gson
 
+class RegistrationPresenter {
+    lateinit var iRegisterView: IRegisterView
+    fun makeRequest(userRequest: UserRequest) {
+        val auth = AuthService()
+        auth.register(userRequest,
+            onSuccess = {
+                val body = it?.body?.string().toString()
+                val registerResponse = Gson().fromJson(body, RegisterResponse::class.java)
+                iRegisterView.onSuccess(registerResponse)
+            },
+            onFailure = { iRegisterView.onFailure(it) })
+    }
 }
