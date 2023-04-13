d645af12713826045855948e0e4fe2b89330dc90
nahed <nahedshehata>
2023-04-14 01:33:51 +0200

    add personalTask function

--- app/src/main/java/com/chocolatecake/todoapp/data/model/response/PersonalTaskResponse.kt
+++ app/src/main/java/com/chocolatecake/todoapp/data/model/response/PersonalTaskResponse.kt
@@ -1,7 +1,9 @@
 package com.chocolatecake.todoapp.data.model.response
 
+import com.google.gson.annotations.SerializedName
+
 data class PersonalTaskResponse(
-    val tasksListPerson: List<PersonTaskRequset>?,
+    @SerializedName("value") val tasksListPerson: List<PersonTaskRequset>?,
     val message: String?,
     val isSuccess: String?
 )
--- app/src/main/java/com/chocolatecake/todoapp/ui/TaskDetailsFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/TaskDetailsFragment.kt
@@ -6,33 +6,31 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.UiThread
 import com.chocolatecake.todoapp.R
 import com.chocolatecake.todoapp.data.local.TaskSharedPreferences
-import com.chocolatecake.todoapp.data.model.response.TeamTaskResponse
+import com.chocolatecake.todoapp.data.model.response.PersonalTaskResponse
 import com.chocolatecake.todoapp.data.model.response.TeamTasksResponse
+import com.chocolatecake.todoapp.data.network.services.personal.PersonalTaskService
 import com.chocolatecake.todoapp.data.network.services.team.TeamTaskService
 import com.chocolatecake.todoapp.databinding.FragmentTaskDetailsBinding
 import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
 import com.google.gson.Gson
 
 class TaskDetailsFragment() : BaseFragment<FragmentTaskDetailsBinding>() {
+    private val isPersonalType by lazy { arguments?.getBoolean(IS_PERSONAL, true) }
+    private val idTask by lazy { arguments?.getString(TASK_ID) }
     private val preferences by lazy { TaskSharedPreferences() }
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentTaskDetailsBinding =
         FragmentTaskDetailsBinding::inflate
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initializePreferences(requireContext())
-        preferences.token =
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJodHRwczovL3RoZS1jaGFuY2Uub3JnLyIsInN1YiI6ImUxZDg4OTlkLWUxMmMtNGZmMS1hMjNiLTA3MjMzYTIyNjYzNyIsInRlYW1JZCI6IjdjMzBhMDQwLTFiYWQtNDk2Ni1hN2YxLTZhZjk4ZGMzZmFiMyIsImlzcyI6Imh0dHBzOi8vdGhlLWNoYW5jZS5vcmcvIiwiZXhwIjoxNjgxNDk1OTE2fQ.ETN-68eaYCheMeWry6DdAkcVYXhZhAg8cKRVmCu9_Lw"
-        getTaskById(requireContext(), "4559e4d7-27e5-4404-b757-a4f783400654")
+        getTask()
+
 
     }
 
-    private fun getTaskById(context: Context, id: String) {
-
-
+    private fun getTeamTaskById(id: String) {
         val tasks = TeamTaskService(preferences).getAllTasks(
             onSuccess = {
                 requireActivity().runOnUiThread() {
@@ -43,33 +41,70 @@ class TaskDetailsFragment() : BaseFragment<FragmentTaskDetailsBinding>() {
                         textViewTitle.text = taskContent?.titleTeamTask
                         textViewAssignee.text = taskContent?.assignee
                         textViewDescription.text = taskContent?.descriptionTeamTask
-                        textViewStatus.text = getStatusByNum(taskContent!!.statusTeamTask).toString()
+                        textViewStatus.text =
+                            getStatusByNum(taskContent!!.statusTeamTask).toString()
                     }
                 }
             },
             onFailure = {
                 Log.i("allTasks", "fail $it")
+
             }
 
         )
     }
 
-    private fun getStatusByNum(num: Int):String {
+    private fun getPersonalTaskById(id: String) {
+        val tasks = PersonalTaskService(preferences).getAllTasks(
+            onSuccess = {
+                requireActivity().runOnUiThread() {
+                    val result =
+                        Gson().fromJson(
+                            it.body?.string().toString(),
+                            PersonalTaskResponse::class.java
+                        )
+                    with(binding) {
+                        val taskContent = result.tasksListPerson?.find { it.idPersonalTask == id }
+                        textViewTitle.text = taskContent?.titlePersonalTask
+                        textViewDescription.text = taskContent?.descriptionPersonalTask
+                        textViewStatus.text =
+                            getStatusByNum(taskContent!!.statusPersonTask).toString()
+                    }
+                }
+            },
+            onFailure = {
+                Log.i("allTasks", "fail $it")
+
+            }
+
+        )
+    }
+
+    private fun getTask() {
+        if (isPersonalType == true) {
+            idTask?.let { getPersonalTaskById(it) }
+        } else {
+            idTask?.let { getTeamTaskById(it) }
+        }
+    }
+
+    private fun getStatusByNum(num: Int): String {
         return when (num) {
             0 -> getString(R.string.status_todo)
             1 -> getString(R.string.status_inprogress)
             2 -> getString(R.string.status_done)
-            else -> "unkown"
+            else -> "unknown"
         }
     }
 
     private fun initializePreferences(context: Context) = preferences.initPreferences(context)
-
     companion object {
         private const val TASK_ID = "Task Id"
-        fun newInstance(taskId: String) =
+        private const val IS_PERSONAL = "Is_Personal"
+        fun newInstance(taskId: String, isPersonal: Boolean) =
             TaskDetailsFragment().apply {
                 arguments = Bundle().apply {
+                    putBoolean(IS_PERSONAL, isPersonal)
                     putString(TASK_ID, taskId)
                 }
             }
