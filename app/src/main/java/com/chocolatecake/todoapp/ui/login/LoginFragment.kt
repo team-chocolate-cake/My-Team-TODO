5a5ed0e381bf7941c56b966c0e9239341a10e43f
jinan <jinanaqb@gmail.com>
2023-04-14 15:15:25 +0300

    fix progress bar

--- app/src/main/java/com/chocolatecake/todoapp/ui/login/LoginFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/login/LoginFragment.kt
@@ -1,24 +1,16 @@
 package com.chocolatecake.todoapp.ui.login
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.TextView
-import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doOnTextChanged
 import com.chocolatecake.todoapp.data.model.request.UserRequest
 import com.chocolatecake.todoapp.databinding.FragmentLoginBinding
 import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
 import com.chocolatecake.todoapp.ui.home.HomeFragment
 import com.chocolatecake.todoapp.ui.login.presenter.LoginPresenter
-import com.chocolatecake.todoapp.ui.register.RegisterFragment
 import com.chocolatecake.todoapp.util.*
-import com.google.android.material.snackbar.Snackbar
 
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
@@ -44,21 +36,26 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
                 username = binding.editTextUsername.text.toString().trim(),
                 password = binding.editTextInputPassword.text.toString().trim(),
             )
-            if (userRequest.username.isNotEmpty() && userRequest.password.isNotEmpty()) {
-                presenter.clickableLoginButton(userRequest)
-                showProgressBar()
-            } else {
-                requireActivity().showSnackbar(message = "fill fields please" ,binding.root)
+
+            userRequest.apply {
+                if (username.isNotEmpty() && password.isNotEmpty()) {
+                    if (!username.usernameLength() && !password.passwordLength()) {
+                        presenter.clickableLoginButton(this)
+                        showProgressBar()
+                    }
+                } else {
+                    requireActivity().showSnackbar(message = "fill fields please", binding.root)
+                }
             }
         }
     }
 
-    fun showProgressBar(){
+    fun showProgressBar() {
         binding.buttonLogin.hide()
         binding.progressBar.show()
     }
 
-    fun hideProgressBar(){
+    fun hideProgressBar() {
         binding.progressBar.hide()
         binding.buttonLogin.show()
     }
@@ -78,7 +75,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
     private fun checkPasswordValidate() {
         binding.editTextInputPassword.apply {
             doOnTextChanged { text, start, before, count ->
-               if (text.toString().passwordLength() && text.toString().isNotEmpty()) {
+                if (text.toString().passwordLength() && text.toString().isNotEmpty()) {
                     binding.textViewPasswordValidate.show()
                 } else {
                     binding.textViewPasswordValidate.hide()
@@ -89,7 +86,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
 
     override fun onFailure(message: String?) {
         requireActivity().runOnUiThread {
-            requireActivity().showSnackbar(message = message ,binding.root)
+            requireActivity().showSnackbar(message = message, binding.root)
             hideProgressBar()
         }
     }
@@ -100,7 +97,7 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(), LoginView {
 
     private fun onClickRegisterButton() {
         binding.textViewRegisterBody.setOnClickListener {
-            requireActivity().navigateTo(RegisterFragment())
+            //requireActivity().navigateTo(RegisterFragment())
         }
     }
 }
