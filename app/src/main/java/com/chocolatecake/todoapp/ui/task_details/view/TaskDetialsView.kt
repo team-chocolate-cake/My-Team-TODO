48a11c74ba590adf196856788138f70a43bf00ef
Saif Alhaider <saifharith90@gmail.com>
2023-04-15 13:25:11 +0300

    edit:changed object

--- app/src/main/java/com/chocolatecake/todoapp/ui/task_details/view/TaskDetialsView.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/task_details/view/TaskDetialsView.kt
@@ -1,9 +1,9 @@
-package com.chocolatecake.todoapp.ui.home.view
+package com.chocolatecake.todoapp.ui.task_details.view
 
 import com.chocolatecake.todoapp.data.model.response.*
 interface TaskDetailsView {
   fun setDataTeam(result: TeamTask?)
-  fun setDataPersonal(result: PersonTaskRequset?)
+  fun setDataPersonal(result: PersonalTask?)
   fun onCreateTaskFailure()
   fun onCreateTaskSuccess()
 }
