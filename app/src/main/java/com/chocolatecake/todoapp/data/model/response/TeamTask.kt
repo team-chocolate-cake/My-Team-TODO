8043b150b6f7fbfd7d47eebb70829a56c11774a6
Saif Alhaider <saifharith90@gmail.com>
2023-04-15 13:23:53 +0300

    add:parcelize

--- app/src/main/java/com/chocolatecake/todoapp/data/model/response/TeamTask.kt
+++ app/src/main/java/com/chocolatecake/todoapp/data/model/response/TeamTask.kt
@@ -1,7 +1,10 @@
 package com.chocolatecake.todoapp.data.model.response
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class TeamTask(
     @SerializedName ("id") val idTeamTask: String,
     @SerializedName("title") val titleTeamTask: String,
@@ -9,4 +12,4 @@ data class TeamTask(
     val assignee: String,
     @SerializedName ("status") val statusTeamTask: Int,
     val creationTime: String
-)
+):Parcelable
--- app/src/main/java/com/chocolatecake/todoapp/ui/task_details/view/TaskDetailsFragment.kt
+++ app/src/main/java/com/chocolatecake/todoapp/ui/task_details/view/TaskDetailsFragment.kt
@@ -1,45 +1,46 @@
 package com.chocolatecake.todoapp.ui.task_details.view
 
+import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import com.chocolatecake.todoapp.R
-import com.chocolatecake.todoapp.data.model.response.PersonTaskRequset
+import com.chocolatecake.todoapp.data.model.response.PersonalTask
 import com.chocolatecake.todoapp.data.model.response.TeamTask
 import com.chocolatecake.todoapp.databinding.FragmentTaskDetailsBinding
 import com.chocolatecake.todoapp.ui.base.fragment.BaseFragment
 import com.chocolatecake.todoapp.ui.home.view.TaskDetailsView
 import com.chocolatecake.todoapp.ui.task_details.presenter.TaskDetailsPresenter
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 class TaskDetailsFragment() : BaseFragment<FragmentTaskDetailsBinding>(), TaskDetailsView {
+
     private val taskDetailsPresenter: TaskDetailsPresenter by lazy {
         TaskDetailsPresenter(requireContext())
     }
 
-    private val isPersonalType by lazy {
-        arguments?.getBoolean(IS_PERSONAL, false)
-    }
-    private val idTask by lazy { arguments?.getString(TASK_ID) }
     override val inflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentTaskDetailsBinding =
         FragmentTaskDetailsBinding::inflate
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         taskDetailsPresenter.taskDetailsView = this
-         getTask()
+        getTask()
     }
 
+
     private fun getTask() {
-        if (isPersonalType == true) {
-            idTask?.let { taskDetailsPresenter.getPersonalTaskById(it) }
-        } else {
-            idTask?.let { taskDetailsPresenter.getTeamTaskById(it) }
-            Log.d(
-                "hi",
-                "${taskDetailsPresenter.getTeamTaskById("7ee7e2eb-223f-4d71-bbcb-a3b75c7cb13e")}"
-            )
+        val isPersonal = arguments?.getBoolean(IS_PERSONAL, true)!!
+        if (isPersonal) {
+            val personalResult =
+                arguments?.getParcelable(PERSONAL_TASK_OBJECT, PersonalTask::class.java)
+            setDataPersonal(personalResult)
+        }else{
+            val teamResult =
+                arguments?.getParcelable(TEAM_TASK_OBJECT, TeamTask::class.java)
+            setDataTeam(teamResult)
         }
     }
 
@@ -50,18 +51,6 @@ class TaskDetailsFragment() : BaseFragment<FragmentTaskDetailsBinding>(), TaskDe
             2 -> getString(R.string.status_done)
             else -> "unknown"
         }
-    }
-
-    companion object {
-        private const val TASK_ID = "44210916-1b5c-4849-87fd-8761fca60827"
-        private const val IS_PERSONAL = "Is_Personal"
-        fun newInstance(taskId: String, isPersonal: Boolean) =
-            TaskDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putBoolean(IS_PERSONAL, isPersonal)
-                    putString(TASK_ID, taskId)
-                }
-            }
     }
 
     override fun setDataTeam(result: TeamTask?) {
@@ -75,12 +64,12 @@ class TaskDetailsFragment() : BaseFragment<FragmentTaskDetailsBinding>(), TaskDe
         }
     }
 
-    override fun setDataPersonal(result: PersonTaskRequset?) {
+    override fun setDataPersonal(result: PersonalTask?) {
         requireActivity().runOnUiThread {
             with(binding) {
                 textViewTitle.text = result?.titlePersonalTask
                 textViewDescription.text = result?.descriptionPersonalTask
-                textViewStatus.text = result?.statusPersonTask?.let { getStatusByNum(it) }
+                textViewStatus.text = result?.statusPersonalTask?.let { getStatusByNum(it) }
             }
         }
     }
@@ -91,5 +80,28 @@ class TaskDetailsFragment() : BaseFragment<FragmentTaskDetailsBinding>(), TaskDe
 
     override fun onCreateTaskSuccess() {
         TODO("Not yet implemented")
+    }
+
+    companion object {
+        private const val TEAM_TASK_OBJECT = "Team_Task"
+        private const val PERSONAL_TASK_OBJECT = "Personal_Task"
+        private const val IS_PERSONAL = "Is_Personal"
+        fun newTeamInstance(teamTask: TeamTask) {
+            TaskDetailsFragment().apply {
+                arguments = Bundle().apply {
+                    putBoolean(IS_PERSONAL, false)
+                    putParcelable(TEAM_TASK_OBJECT, teamTask)
+                }
+            }
+        }
+
+        fun newPersonalInstance(personalTask: PersonalTask) {
+            TaskDetailsFragment().apply {
+                arguments = Bundle().apply {
+                    putBoolean(IS_PERSONAL, true)
+                    putParcelable(PERSONAL_TASK_OBJECT, personalTask)
+                }
+            }
+        }
     }
 }
