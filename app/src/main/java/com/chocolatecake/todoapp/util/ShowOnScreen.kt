c183059aab7e4f2534ed22d0bc8f2b6be522e065
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-04-15 18:09:07 +0200

    add string status

--- app/src/main/java/com/chocolatecake/todoapp/util/ShowOnScreen.kt
+++ app/src/main/java/com/chocolatecake/todoapp/util/ShowOnScreen.kt
@@ -1,10 +1,11 @@
 package com.chocolatecake.todoapp.util
 import android.view.View
 import androidx.fragment.app.FragmentActivity
+import com.chocolatecake.todoapp.R
 import com.google.android.material.snackbar.Snackbar
 
 fun FragmentActivity.showSnackbar(message: String?, view: View){
     Snackbar.make(view, message.toString(), Snackbar.LENGTH_LONG)
-        .setAction("Ok") {}
+        .setAction(getString(R.string.ok)) {}
         .show()
 }
