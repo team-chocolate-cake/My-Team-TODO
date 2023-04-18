3983e4e5b41791860bad0edf719821dd6d812f75
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-04-19 01:02:03 +0200

    add fade to toggle view

--- app/src/main/java/com/chocolatecake/todoapp/core/util/Extention.kt
+++ app/src/main/java/com/chocolatecake/todoapp/core/util/Extention.kt
@@ -1,12 +1,19 @@
 package com.chocolatecake.todoapp.core.util
 
+import android.transition.Fade
+import android.transition.TransitionManager
 import android.view.View
+import android.view.ViewGroup
 
 fun View.show() {
+    val transition = Fade()
+    TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
     this.visibility = View.VISIBLE
 }
 
 fun View.hide() {
+    val transition = Fade()
+    TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
     this.visibility = View.GONE
 }
 
