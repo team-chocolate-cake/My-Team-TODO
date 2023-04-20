64926fdf9476df4a1695d4d652b3c6acd0211d8c
Mohammed Sayed <83292287+Mohammedbih@users.noreply.github.com>
2023-04-20 02:55:39 +0200

    solve issue

--- app/src/main/java/com/chocolatecake/todoapp/core/util/ViewExtention.kt
+++ app/src/main/java/com/chocolatecake/todoapp/core/util/ViewExtention.kt
@@ -1,18 +1,25 @@
 package com.chocolatecake.todoapp.core.util
 
+import android.app.Activity
 import android.transition.Fade
 import android.transition.TransitionManager
 import android.view.View
 import android.view.ViewGroup
 
 fun View.show() {
-    val transition = Fade()
-    TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
-    this.visibility = View.VISIBLE
+    val activity = context as Activity
+    activity.runOnUiThread {
+        val transition = Fade()
+        TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
+        this.visibility = View.VISIBLE
+    }
 }
 
 fun View.hide() {
-    val transition = Fade()
-    TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
-    this.visibility = View.GONE
+    val activity = context as Activity
+    activity.runOnUiThread {
+        val transition = Fade()
+        TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
+        this.visibility = View.GONE
+    }
 }
